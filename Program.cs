387839c64d524d0490655ff038895538b2d75721
Maxym Fediw <362706764@GoTVDSB.ca>
2026-03-11 13:18:51 -0400

    Checkpoint #1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,100 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           //Maxym F.
+            
+            Part2();
+        }
+
+        public static void Part1() 
+        {
+            //Part 1-Reading In Colours
+
+            Random Numbers = new Random();
+            Numbers.Next(1, 5);
+            List<string> colors = new List<string>();
+            Console.WriteLine("I'm going to ask you for five different colours. I will then randomly display one of these colours.");
+            Console.WriteLine();
+            for (int i = 0; i < 5; i++) 
+            {
+
+                Console.WriteLine("Give me a colour!");
+                colors.Add(Console.ReadLine());
+
+            }
+
+            Console.WriteLine();
+            
+            Console.WriteLine(string.Join(", " , colors));
+            Console.WriteLine();
+
+            Console.WriteLine("Here's a randomly selected colour from your list you gave me:");
+            Console.WriteLine(colors[Numbers.Next(colors.Count)]);
+
+        }
+
+        public static void Part2() 
+        {
+            //Part 2-Random Numbers
+
+            int usersImput, amountNums, max, min, between, count = 0;
+            
+
+            Random gen = new Random();
+           
+            List<int> numbers = new List<int>();
+
+            Console.WriteLine("Think of a list of numbers. How many numbers are there?");
+            int.TryParse(Console.ReadLine(), out amountNums);
+
+            Console.WriteLine("What is the highest number?");
+            int.TryParse(Console.ReadLine(), out max);
+
+            Console.WriteLine("What is the lowest number?");
+            int.TryParse(Console.ReadLine(), out min);
+
+            for (int i = 1; i <= amountNums; i++)
+            {
+                numbers.Add(gen.Next(min, max +1));
+            }
+                
+            Console.WriteLine(string.Join(", " , numbers));
+
+            Console.WriteLine("Give me a number between " + min + " and " + max + ".");
+            int.TryParse(Console.ReadLine(), out between);
+
+            for (int i = 0; i < numbers.Count; i++) 
+            {
+                if (numbers[i] == between) 
+                {
+                    count += 1;
+                }
+
+            }
+
+            Console.WriteLine(string.Join(", ", between));
+            Console.WriteLine("There are " + count + " " + between + "'s in this sequence.");
+
+            Console.WriteLine("Lets make " + between + " equal zero.");
+            for (int i = 0; i < numbers.Count; i++) 
+            {
+                if (numbers[i] == between) 
+                {
+                    numbers[i] = 0;
+                }
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(string.Join(", ", numbers));
+
+
+
+
+
+
+
+
         }
     }
 }
