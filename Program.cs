9d6808c525be8842727bc914242ee011b3e5f926
Maxym Fediw <362706764@GoTVDSB.ca>
2026-03-13 12:40:05 -0400

    All Done!

--- Program.cs
+++ Program.cs
@@ -40,12 +40,15 @@
         {
             //Part 2-Random Numbers
 
-            int usersImput, amountNums, max, min, between, count = 0;
-            
+            int usersImput, amountNums, max, min, between, count, wrong;
+
+            count = 0;
 
             Random gen = new Random();
            
             List<int> numbers = new List<int>();
+
+           
 
             Console.WriteLine("Think of a list of numbers. How many numbers are there?");
             int.TryParse(Console.ReadLine(), out amountNums);
@@ -56,6 +59,16 @@
             Console.WriteLine("What is the lowest number?");
             int.TryParse(Console.ReadLine(), out min);
 
+            if (max < min)
+            {
+                wrong = min;
+
+                min = max;
+
+                max = wrong;
+
+            }
+
             for (int i = 1; i <= amountNums; i++)
             {
                 numbers.Add(gen.Next(min, max +1));
@@ -63,7 +76,7 @@
                 
             Console.WriteLine(string.Join(", " , numbers));
 
-            Console.WriteLine("Give me a number between " + min + " and " + max + ".");
+            Console.WriteLine("Give me a number between " + min + " and " + max + " that you can see above.");
             int.TryParse(Console.ReadLine(), out between);
 
             for (int i = 0; i < numbers.Count; i++) 
@@ -78,6 +91,8 @@
             Console.WriteLine(string.Join(", ", between));
             Console.WriteLine("There are " + count + " " + between + "'s in this sequence.");
 
+            Console.WriteLine();
+
             Console.WriteLine("Lets make " + between + " equal zero.");
             for (int i = 0; i < numbers.Count; i++) 
             {
@@ -91,7 +106,37 @@
             Console.WriteLine();
             Console.WriteLine(string.Join(", ", numbers));
 
+            Console.WriteLine("Lets make all of the numbers equal to zero now!");
 
+            Console.WriteLine() ;
+
+            for (int i = 0; i < numbers.Count; i++) 
+            {
+                numbers[i] = 0;
+            }
+            
+            Console.WriteLine(string.Join(", ", numbers));
+
+            Console.WriteLine();
+
+            Console.WriteLine("Lets generate some new numbers!");
+
+            Console.WriteLine();
+            
+            for (int i = 1; i <= amountNums; i++)
+            {
+
+                numbers.Remove(gen.Next(0,1));
+            }
+
+
+            for (int i = 1; i <= amountNums; i++)
+            {
+                
+                numbers.Add(gen.Next(min, max + 1));
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
 
 
 
